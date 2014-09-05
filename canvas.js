aff0b0c5409dd6982fe35a6e7cf80321967149fb
estebanrfp <estebanrfp@gmail.com>
2014-09-05 16:59:42 +0200

    dibujamos linea

--- canvas.js
+++ canvas.js
@@ -1,7 +1,11 @@
 var dibujo, lienzo;
 function inicio()
 {
-	dibujo =  document.getElementById("dibujito");
-	lienzo = dibujo.getContext("2d"); 
+	dibujo =  document.getElementById("dibujito"); 
+	lienzo = dibujo.getContext("2d"); //obtengo el contexto bidireccionar
+	lienzo.moveTo(100,100); //arranco en coordenadas 100, 100
+	lienzo.lineTo(200,100); //muevo hasta 200,100
+	lienzo.strokestyle = "green"; //defino el color del trazo
+	lienzo.stroke; //dibujo
 
 }
